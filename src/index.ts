dcef42cff248feea5403403f768c25669a07d38d
Davenn69 <devendeven98@gmail.com>
2026-04-06 16:36:57 +0700

    deleted : unwanted config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import app from "./app";
 import dotenv from "dotenv";
-import { drizzle } from "drizzle-orm/postgres-js";
 
 dotenv.config();
 const PORT = process.env.PORT;
-const connectionString = process.env.DATABASE_URL!;
 
 process.on("unhandledRejection", (reason, promise) => {
   console.error("Unhandled Rejection at:", promise, "reason:", reason);
