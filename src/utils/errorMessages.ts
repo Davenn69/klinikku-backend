a7ac3f1db407451a00bea6ac7d62d312dc05fa2d
Davenn69 <devendeven98@gmail.com>
2026-04-07 09:08:45 +0700

    added : login and register handlers

--- src/utils/errorMessages.ts
+++ src/utils/errorMessages.ts
@@ -1,5 +1,14 @@
 class ErrorMessages {
   notFound: string = "route not found";
+
+  //Auth related
+  missingRegisterParameters: string = "Name, email, and password are required.";
+  missingLoginParameters: string = "Email and password are required.";
+  passwordLength: string = "Password must be at least 8 characters long.";
+  emailAlreadyExists: string = "Email is already registered.";
+  failedRegisterUser: string = "Failed to create user.";
+  invalidLoginCredentials: string = "Invalid email or password.";
+  accountInactive: string = "This account is inactive.";
 }
 
 export const errors = new ErrorMessages();
